d090eae13c4240be96438b38c4f8dadac70abba3
Luigi Lesca <luigilesca@hotmail.it>
2022-11-10 19:55:43 +0100

    create header and install react-icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import { AiOutlinePlusCircle } from 'react-icons/ai';
 import todoLogo from '../../assets/todoLogo.svg';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src={todoLogo} />{' '}
+      <img src={todoLogo} />
+
+      {/* Form */}
+      <form className={styles.newTaskForm} action=''>
+        <input type='text' placeholder='Add new task' />
+        <button>
+          Create
+          <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
     </header>
   );
 };
